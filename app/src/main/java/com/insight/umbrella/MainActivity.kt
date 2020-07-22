a69430c9cfdf3d62586b74b121c35b67077b5986
sydneyq <qrsydney@gmail.com>
2020-07-22 13:04:31 -0400

    trying speech to text

--- app/src/main/java/com/insight/umbrella/MainActivity.kt
+++ app/src/main/java/com/insight/umbrella/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.sydneyq.umbrella
 
+import android.app.Activity
 import android.os.Bundle
+import android.speech.RecognizerIntent
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_CODE_SPEECH_INPUT = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +24,37 @@ class MainActivity : AppCompatActivity() {
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
+        }
+    }
+
+    private fun speak(){
+        // speech to text: https://www.youtube.com/watch?v=Wv2hafcjEVY
+        val mIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        mIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+        mIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+        mIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, value="Hello")
+
+        try{
+            // if there's no error then show speech to text dialog
+            startActivityForResult(mIntent, REQUEST_CODE_SPEECH_INPUT)
+        }
+        catch(e: Exception){
+            // if there's an error, error message
+            Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun onActivityResult(requestCode:Int, resultCode:Int, data:Intent?){
+        super.onActivityResult(requestCode, resultCode, data)
+        when(requestCode){
+            REQUEST_CODE_SPEECH_INPUT -> {
+                if (resultCode == Activity.RESULT_OK && null != data){
+                    // get text from result
+                    val result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                    // set the text to textview
+                    textTv.text = result[0]
+                }
+            }
         }
     }
 
